976841915c8b08068fcc38e863095d06c240f2a3
tdat1037 <pktrum3@gmail.com>
2025-04-14 23:00:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Đúng đúng ',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại nch với anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'ê nha ê nha =)))',
+    messDesc: 'Thôi chắc vậy đủ rồi sợ nó hơi quá :3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/tuandat.037' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
